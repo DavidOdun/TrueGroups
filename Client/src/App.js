422575cadf6d4ccc0c081b5a3532c409d97394d0
DavidOdun <seun@davids-mbp.dhcp.nd.edu>
2020-02-17 21:24:28 -0500

    Intial Commit of Server, Client:Connecting React FrontEnd, Node and Express Backend and Postgress Database

---
+++ Client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+/* Starting Home Page */
+class App extends Component {
+  state = {
+    hello: null
+  }
+
+  componentDidMount(){
+    axios.get('/hello')
+      .then(res => this.setState({hello: res.data}))
+      .catch(err => console.log(err))
+      console.log("We got the state")
+      console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+      {
+        this.state.hello
+          ? <div> {this.state.hello} </div>
+          : null }
+      </div>
+    );
+  }
+}
+
+export default App;
