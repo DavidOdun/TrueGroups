025878c12db7b08e5876f208a5f29e141cbc9d81
DavidOdun <seun@Davids-MacBook-Pro.local>
2020-02-26 14:14:52 -0500

    Edited the home page and the index.js file to add buttons

--- Client/src/components/HomePage.js
+++ Client/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button} from 'reactstrap';
 
 class HomePage extends Component {
     state = {
@@ -15,8 +16,28 @@ class HomePage extends Component {
   
     render() {
       return (
-        <div>
-        </div>
+            <div>
+                <nav className="navbar navbar-light bg-light justify-content-between">
+                    <a className="navbar-brand" href="#">
+                        <img src="/TrueGroupLogo.png" width="150" height="150" alt="True Group Company Logo">
+                        </img>
+                    </a>
+                    <h1>Welcome to True Groups</h1>
+                    <div>
+                        <Button size="lg" color="primary"> Register</Button>{' '}
+                        <Button size="lg" color="secondary"> Login </Button>
+                    </div>
+                </nav>
+                <div class="row justify-content-center">
+                    <div class="col">
+                        One of two columns
+                    </div>
+                    <div class="col">
+                        <img src="/StudentsWorking.jpg" class="img-fluid" alt="Responsive image">
+                        </img>                          
+                    </div>
+                </div>
+            </div>
       );
     }
   }
