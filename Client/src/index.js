f5ba8e538cdb0b5a1678e02fb2a7153393ac0e3d
DavidOdun <seun@davids-mbp.dhcp.nd.edu>
2020-02-26 19:08:05 -0500

    Changed the index file to load the App.js component as the initial page

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-//import HomePage from './components/HomePage';
-//import SignUpPage from './components/SignUpPage';
-import SignInPage from './components/SignInPage';
-
-ReactDOM.render(<SignInPage />, document.getElementById('root'));
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
