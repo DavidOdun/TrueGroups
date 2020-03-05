21d74f50b4c114098bd5d5a6fb587c3c219aa0c1
eddie <ecunnin2@nd.edu>
2020-03-05 13:34:59 -0500

    /api/

--- Client/src/App.js
+++ Client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    axios.get('/hello')
+    axios.get('/api/hello')
       .then(res => this.setState({hello: res.data}))
       .catch(err => console.log(err))
       console.log("We got the state")
