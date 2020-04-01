93e768f697e9c2e64257bde70e90f41a98fd6889
DavidOdun <seun@Davids-MBP.attlocal.net>
2020-03-31 23:04:35 -0500

    Created function to make api call for getting functions. Implemented the feature for the user to add multiple questions and improved the modal for user flow

--- Client/src/components/ProfessorPage.js
+++ Client/src/components/ProfessorPage.js
@@ -1,23 +1,84 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
-import { Button, UncontrolledTooltip } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Label, UncontrolledTooltip } from 'reactstrap';
 import { Redirect } from 'react-router'
+import axios from 'axios';
 
 class ProfessorPage extends Component {
     constructor(props)
     {
         super(props)        
         this.state = {
+            surveyQuestions: [],
             pageRedirect: "",
+            userAddedQuestions: "",
             userInfo: props.location.state.user_data
         }
     }
 
     componentDidMount()
     {
+        /* API Call to get user Survey Questions ---> axios.get('/api/v1/questions/all') */
+        this.apiCallGetQuestions();
 
     }
 
+    /* Create a fuction to get the axios call for questions */
+    apiCallGetQuestions()
+    {
+        axios.get('/api/v1/questions/all')
+            .then(qRes => {
+                if (qRes.data.length !== 0)
+                {
+                    this.setState({surveyQuestions: qRes.data, userAddedQuestions: ""})
+                }
+            });
+    }
+    
+    apiCallAddQuestions()
+    {
+        let filteredQuestion = this.state.userAddedQuestions.split("\n").filter(item => item);
+        console.log(filteredQuestion)
+
+        for (var pos = 0; pos < filteredQuestion.length; pos++)
+        {
+            let tempForm = {
+                "question_string": filteredQuestion[pos]
+            } 
+            axios.post('/api/v1/questions/add',tempForm)
+                .then(addResponse => {
+                    console.log("Response Below")
+                    console.log(addResponse)
+                })
+                .catch(error => {
+                    console.log(error.response)
+                });
+        }
+
+        alert("Submit Succesfully Completed!")
+        this.apiCallGetQuestions()    
+    }
+
+    structureModalQuestions()
+    {
+        const userInputItems = []
+        const formGroupItems = []
+        
+        for (var pos = 0; pos < this.state.surveyQuestions.length; pos++)
+        {
+            var temp = pos+1;
+            let qName = "Q"+temp;
+
+            formGroupItems.push
+            (
+                <FormGroup key={pos}> 
+                    <Label for={qName}>{qName + ": " + this.state.surveyQuestions[pos].question_string}</Label>
+                </FormGroup>
+            )
+        }
+        userInputItems.push(<Form key={0}>{formGroupItems}</Form>);
+        return userInputItems;
+    }
 
     render() {
         if (this.state.pageRedirect)
@@ -43,7 +104,6 @@ class ProfessorPage extends Component {
                     <div>
                         {/* Buttons: 1. Edit Profile Page, 2. Edit Survey 3. Logout*/}
                         <Button size="lg" color="primary" onClick={() => this.setState({pageRedirect: "/editprofile"})}> Edit Profile</Button>{' '}
-                        <button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target=".bd-example-modal-lg">Edit Survey</button>{' '}
                         <Button size="lg" color="secondary" onClick={() => this.setState({pageRedirect: "/"})}> Logout </Button>
 
                     </div>
@@ -57,11 +117,25 @@ class ProfessorPage extends Component {
                         <button className="btn btn-primary btn-lg btn-block" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Create New Class Room</button>
                     </div>
                 </div>
+
                 <div className="row">
                     <div className="col">
                         <div className="collapse multi-collapse" id="multiCollapseExample1">
                             <div className="card card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                { this.state.surveyQuestions.length === 0 ? 
+                                    <h3>No Current Questions. Use the Add button below to begin </h3> : 
+                                    <h3>{this.structureModalQuestions()}</h3>
+                                }                                
+                            </div>
+                            <div className="card-footer">
+                                <div className="row">
+                                    <div className="col">
+                                        <button type="button" className="btn btn-success btn-lg btn-block" data-toggle="modal" data-target=".addQ" >Add Questions</button>{' '}
+                                    </div>
+                                    <div className="col">
+                                        <button type="button" className="btn btn-danger btn-lg btn-block" data-toggle="modal" data-target=".removeQ">Remove Questions</button>{' '}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -73,6 +147,49 @@ class ProfessorPage extends Component {
                         </div>
                     </div>
                 </div>
+
+                <div className="modal fade addQ" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLongTitle">User Survey Questions</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <Label for="addQuestion">Enter Question(s) Below. SEPERATE MULTIPLE QUESTION WITH [ENTER]</Label>
+                                <Input type="textarea" name="text" id="addQuestion" onChange={(e) => this.setState({userAddedQuestions: e.target.value})} placeholder= "What color is the sky?\n" />  
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary btn-lg" data-dismiss="modal">Cancel</button>
+                                <button type="button" className="btn btn-primary btn-lg" onClick={() => this.apiCallAddQuestions()} data-dismiss="modal">Submit Changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+
+                 <div className="modal fade removeQ" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLongTitle">User Survey Questions</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                RemoveQ: We gon call a function here to get user input
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary btn-lg" data-dismiss="modal">Cancel</button>
+                                <Button size="lg" color="primary" onClick={() => this.handleButtonPress("submitsurvey")}> Submit Changes </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>                                   
+
+
             </div>
       );
     }
