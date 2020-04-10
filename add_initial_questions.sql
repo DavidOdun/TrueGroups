01848acb2f97c936d300ed1e75141d7d9f0f0fe3
Allen Duong <allenduong@Allens-MBP.frontierlocal.net>
2020-04-10 13:36:29 -0500

    mend

--- add_initial_questions.sql
+++ add_initial_questions.sql
@@ -1,14 +1,14 @@
 INSERT INTO questions (question_id, question_string) 
     VALUES 
-        (1,"You often spend time exploring unrealistic yet intriguing ideas."),
-        (2,"Your plans are more likely to look like a rough list of ideas than a detailed itinerary."),
-        (3,"People can rarely upset you."),
-        (4,"If you have to temporarily put your plans on hold, you make sure it is your top priority to get back on track as soon as possible."),
-        (5,"You are more of a detail-oriented than a big picture person."),
-        (6,"You have a careful and methodical approach to life."),
-        (7,"You can stay calm under a lot of pressure."),
-        (8,"You are dedicated and focused on your goals, only rarely getting sidetracked."),
-        (9,"If you make a mistake, you tend to start doubting yourself, your abilities, or your knowledge."),
-        (10,"When it comes to making life-changing choices, you mostly listen to your heart rather than your head."),
-        (11,"You often make decisions on a whim."),
-        (12,"You always know exactly what you want.");
+        (1,'You often spend time exploring unrealistic yet intriguing ideas.'),
+        (2,'Your plans are more likely to look like a rough list of ideas than a detailed itinerary.'),
+        (3,'People can rarely upset you.'),
+        (4,'If you have to temporarily put your plans on hold, you make sure it is your top priority to get back on track as soon as possible.'),
+        (5,'You are more of a detail-oriented than a big picture person.'),
+        (6,'You have a careful and methodical approach to life.'),
+        (7,'You can stay calm under a lot of pressure.'),
+        (8,'You are dedicated and focused on your goals, only rarely getting sidetracked.'),
+        (9,'If you make a mistake, you tend to start doubting yourself, your abilities, or your knowledge.'),
+        (10,'When it comes to making life-changing choices, you mostly listen to your heart rather than your head.'),
+        (11,'You often make decisions on a whim.'),
+        (12,'You always know exactly what you want.');
