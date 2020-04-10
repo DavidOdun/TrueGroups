eb013f0ea00a60bb8910c05e757c0e607cf636da
Allen Duong <allenduong@Allens-MBP.frontierlocal.net>
2020-04-10 12:05:46 -0500

    Initial Questions SQL File

---
+++ add_initial_questions.sql
@@ -0,0 +1,12 @@
+INSERT INTO questions (question_id, question_string) VALUES (1,"You often spend time exploring unrealistic yet intriguing ideas.");
+INSERT INTO questions (question_id, question_string) VALUES (2,"Your plans are more likely to look like a rough list of ideas than a detailed itinerary.");
+INSERT INTO questions (question_id, question_string) VALUES (3,"People can rarely upset you.");
+INSERT INTO questions (question_id, question_string) VALUES (4,"If you have to temporarily put your plans on hold, you make sure it is your top priority to get back on track as soon as possible.");
+INSERT INTO questions (question_id, question_string) VALUES (5,"You are more of a detail-oriented than a big picture person.");
+INSERT INTO questions (question_id, question_string) VALUES (6,"You have a careful and methodical approach to life.");
+INSERT INTO questions (question_id, question_string) VALUES (7,"You can stay calm under a lot of pressure.");
+INSERT INTO questions (question_id, question_string) VALUES (8,"You are dedicated and focused on your goals, only rarely getting sidetracked.");
+INSERT INTO questions (question_id, question_string) VALUES (9,"If you make a mistake, you tend to start doubting yourself, your abilities, or your knowledge.");
+INSERT INTO questions (question_id, question_string) VALUES (10,"When it comes to making life-changing choices, you mostly listen to your heart rather than your head.");
+INSERT INTO questions (question_id, question_string) VALUES (11,"You often make decisions on a whim.");
+INSERT INTO questions (question_id, question_string) VALUES (12,"You always know exactly what you want.");
